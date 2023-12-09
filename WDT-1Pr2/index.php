5a43ab7c27fe003f81675c490ddd67ebb032ad59
Atharv Naik <114814921+atharvnaik07@users.noreply.github.com>
2023-12-09 13:27:24 +0530

    done

---
+++ WDT-1Pr2/index.php
@@ -0,0 +1,11 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Passing Multiple Variables via URL</title>
+</head>
+<body>
+    <h1>Passing Multiple Variables</h1>
+    <a href="receiver.php?name=Krishna&age=19">Click here to pass 'name' and 'age' variables</a>
+</body>
+</html>
---
+++ WDT-1Pr2/receiver.php
@@ -0,0 +1,5 @@
+<?php
+    $username = $_GET['name'];
+    $age = $_GET['age'];
+    echo "Your name is $username and your age is $age .";
+?>
