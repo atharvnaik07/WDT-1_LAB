2ff95e19b64fed74392800929f30df40680b8f21
Atharv Naik <114814921+atharvnaik07@users.noreply.github.com>
2023-12-09 13:30:00 +0530

    done

---
+++ WDT-1Pr4/function.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User-Defined Function Demo</title>
+</head>
+<body>
+    <h1>User-Defined Function Demo</h1>
+
+    <?php
+    // Define a user-defined function
+    function greet($name) {
+        echo "Hello, $name! Welcome to our website.";
+    }
+
+    // Call the function
+    greet("Krishna");
+    ?>
+
+    <p>Other content on your web page...</p>
+</body>
+</html>
